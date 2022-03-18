74534059b59d04cb25504c1b3863f0d67c9f130d
harshpurohit9763 <70997472+harshpurohit9763@users.noreply.github.com>
2022-03-18 15:17:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+var myImages = ["dice1.png", "dice2.png", "dice3.png", "dice4.png", "dice5.png", "dice6.png"];
+var randomNumber1 = Math.floor(Math.random() * myImages.length + 1);
+
+document.queryselector(".img").setattribute(myImages(randomNumber1));
