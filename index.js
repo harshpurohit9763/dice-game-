d465efb8ace4cd9585135e4fde61aa4aa25019ad
harshpurohit9763 <70997472+harshpurohit9763@users.noreply.github.com>
2022-03-19 00:48:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,37 @@
-var myImages = ["dice1.png", "dice2.png", "dice3.png", "dice4.png", "dice5.png", "dice6.png"];
-var randomNumber1 = Math.floor(Math.random() * myImages.length + 1);
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-document.queryselector(".img").setattribute(myImages(randomNumber1));
+var imageSelect1 = "dice" + randomNumber1 + ".png";
+
+var img_sorce = imageSelect1;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", img_sorce)
+
+// second image
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var imageSelect2 = "dice" + randomNumber2 + ".png";
+
+var img_sorce = imageSelect2;
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", img_sorce)
+
+//condition
+
+if (randomNumber1 > randomNumber2) {
+
+  document.querySelector("h1").innerHTML = "🔥 player 1 wins";
+
+}if (randomNumber1 < randomNumber2) {
+
+  document.querySelector("h1").innerHTML = "🔥 player 2 wins";
+
+} if (randomNumber1 === randomNumber2) {
+
+  document.querySelector("h1").innerHTML = "😲 its a draw";
+
+}
